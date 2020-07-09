782143bd95e17b9093a0279008263a56d144dd41
chichi <djimgoupat@yahoo.fr>
2020-07-09 15:32:50 +0200

    start my own tutotial

---
+++ Tutorial_ErsteSchritteRazorPages/Data/Tutorial_ErsteSchritteRazorPagesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tutorial_ErsteSchritteRazorPages.Modelle;
+
+namespace Tutorial_ErsteSchritteRazorPages.Data
+{
+    public class Tutorial_ErsteSchritteRazorPagesContext : DbContext
+    {
+        public Tutorial_ErsteSchritteRazorPagesContext (DbContextOptions<Tutorial_ErsteSchritteRazorPagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tutorial_ErsteSchritteRazorPages.Modelle.Movie> Movie { get; set; }
+    }
+}
---
+++ Tutorial_ErsteSchritteRazorPages/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Tutorial_ErsteSchritteRazorPages.Data;
+using Tutorial_ErsteSchritteRazorPages.Modelle;
+
+namespace Tutorial_ErsteSchritteRazorPages.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Tutorial_ErsteSchritteRazorPages.Data.Tutorial_ErsteSchritteRazorPagesContext _context;
+
+        public DetailsModel(Tutorial_ErsteSchritteRazorPages.Data.Tutorial_ErsteSchritteRazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
